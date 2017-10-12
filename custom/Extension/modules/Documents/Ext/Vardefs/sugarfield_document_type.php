460220b29823dae5e8b3d6612a27cbd1fab8e96c
jakabadambalazs <jakabadambalazs@gmail.com>
2017-10-12 14:54:11 +0200

    Documents mods

---
+++ custom/Extension/modules/Documents/Ext/Vardefs/sugarfield_document_type.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by Adam Jakab.
+ * Date: 12/10/17
+ * Time: 13.12
+ */
+$dictionary['Document']['fields']['document_type']['type']='enum';
+$dictionary['Document']['fields']['document_type']['options']='document_type_dom';
+$dictionary['Document']['fields']['document_type']['len']='100';
+$dictionary['Document']['fields']['document_type']['required']=1;
+$dictionary['Document']['fields']['document_type']['name']='document_type';
+$dictionary['Document']['fields']['document_type']['vname']='LBL_DOCUMENT_TYPE';
+$dictionary['Document']['fields']['document_type']['massupdate']=0;
+$dictionary['Document']['fields']['document_type']['comments']='';
+$dictionary['Document']['fields']['document_type']['help']='';
+$dictionary['Document']['fields']['document_type']['importable']='true';
+$dictionary['Document']['fields']['document_type']['duplicate_merge']='disabled';
+$dictionary['Document']['fields']['document_type']['duplicate_merge_dom_value']='0';
+$dictionary['Document']['fields']['document_type']['audited']='1';
+$dictionary['Document']['fields']['document_type']['reportable']='true';
+$dictionary['Document']['fields']['document_type']['inline_edit']=false;
+$dictionary['Document']['fields']['document_type']['merge_filter']='disabled';
+$dictionary['Document']['fields']['document_type']['studio']='visible';
+
--- custom/modules/Documents/language/it_IT.lang.php
+++ custom/modules/Documents/language/it_IT.lang.php
@@ -2,4 +2,5 @@
 // created: 2017-10-10 14:32:26
 $mod_strings = array (
   'LBL_CASES_SUBPANEL_TITLE' => 'RAS',
+  'LBL_DOCUMENT_TYPE' => 'Tipo'
 );
