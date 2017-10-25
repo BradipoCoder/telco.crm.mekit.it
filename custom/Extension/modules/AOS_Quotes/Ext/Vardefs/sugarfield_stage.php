f8961628ec094a8370b3e7ac3bb9c9a2fd3c3c42
jakabadambalazs <jakabadambalazs@gmail.com>
2017-10-25 12:12:01 +0200

    working on Documents(Quotes)

---
+++ custom/Extension/modules/AOS_Quotes/Ext/Vardefs/sugarfield_stage.php
@@ -0,0 +1,7 @@
+<?php
+ // created: 2017-10-12 14:54:51
+$dictionary['AOS_Quotes']['fields']['stage']['required']=false;
+$dictionary['AOS_Quotes']['fields']['stage']['inline_edit']=true;
+$dictionary['AOS_Quotes']['fields']['stage']['merge_filter']='disabled';
+
+ ?>
